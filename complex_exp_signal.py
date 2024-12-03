5c24c4b22eae50807b1758753d57b9b258903a2d
heejeankim <heejean.kim@csusb.edu>
2024-12-03 09:52:45 -0800

    update time-axis variable

--- complex_exp_signal.py
+++ complex_exp_signal.py
@@ -3,20 +3,20 @@ import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
 
 # Define the interval for x (arbitrary start and end)
-x_start = -4 * np.pi
-x_end = 6 * np.pi
+t_start = -4 * np.pi
+t_end = 6 * np.pi
 
 # Generate x values within the specified interval
-t = np.linspace(x_start, x_end, 1000)
-Im = np.sin(t)  # y-coordinate (sin curve)
-Re = np.cos(t)  # z-coordinate (cos curve)
+t = np.linspace(t_start, t_end, 1000)
+Re = np.cos(t)  # Real Part
+Im = np.sin(t)  # Imaginary Part
 
 # Define the tick positions (multiples of π within the range)
-tick_positions = np.arange(np.ceil(x_start / np.pi), np.floor(x_end / np.pi) + 1) * np.pi
+tick_positions = np.arange(np.ceil(t_start / np.pi), np.floor(t_end / np.pi) + 1) * np.pi
 
 # Define the tick labels
 tick_labels = []
-for i in range(int(np.ceil(x_start / np.pi)), int(np.floor(x_end / np.pi)) + 1):
+for i in range(int(np.ceil(t_start / np.pi)), int(np.floor(t_end / np.pi)) + 1):
     if i == 0:
         tick_labels.append(r"$0$")
     elif i == 1:
@@ -32,7 +32,7 @@ ax = fig.add_subplot(111, projection='3d')
 
 # Plot the 3D curve
 
-ax.set_xlim(x_start, x_end)
+ax.set_xlim(t_start, t_end)
 ax.set_ylim(-1, 1)
 ax.set_zlim(-1, 1)
 
